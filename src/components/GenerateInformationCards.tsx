356c53f2a0aca862a04e3a257a50ce7472de56de
milliorn <scottmilliorn@gmail.com>
2022-10-28 10:58:28 -0700

    LazyLoadImage

--- src/components/GenerateInformationCards.tsx
+++ src/components/GenerateInformationCards.tsx
@@ -1,4 +1,5 @@
 import uuid from "react-uuid";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export function GenerateInformationCards({
   props,
@@ -10,19 +11,39 @@ export function GenerateInformationCards({
   };
 }): JSX.Element {
   const informationCardsData = [
-    { imgSrc: props.img[0], alt: "image1", pText: props.paragraph[0] },
-    { imgSrc: props.img[1], alt: "image2", pText: props.paragraph[1] },
-    { imgSrc: props.img[2], alt: "image3", pText: props.paragraph[2] },
+    {
+      imgSrc: props.img[0],
+      alt: "image1",
+      pText: props.paragraph[0],
+      width: 1470,
+      height: 980,
+    },
+    {
+      imgSrc: props.img[1],
+      alt: "image2",
+      pText: props.paragraph[1],
+      width: 935,
+      height: 624,
+    },
+    {
+      imgSrc: props.img[2],
+      alt: "image3",
+      pText: props.paragraph[2],
+      width: 935,
+      height: 624,
+    },
   ];
 
   return (
     <div className="gap-16 mx-4 space-y-2 md:space-y-0 md:grid md:grid-cols-3">
       {informationCardsData.map((card) => (
         <div className="w-full hover:shadow-2xl" key={uuid()}>
-          <img
-            src={card.imgSrc}
+          <LazyLoadImage
             alt={card.alt}
             className="border-4 border-blue-900 rounded-xl "
+            height={card.height}
+            src={card.imgSrc}
+            width={card.width}
           />
           <p className="p-2">{card.pText}</p>
         </div>
