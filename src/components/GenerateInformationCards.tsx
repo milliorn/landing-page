4703f43115078622581a476bff375594fc270d75
milliorn <scottmilliorn@gmail.com>
2022-10-28 09:36:11 -0700

    informationCardsData Information data

---
+++ src/components/GenerateInformationCards.tsx
@@ -0,0 +1,30 @@
+export function GenerateInformationCards({
+  props,
+}: {
+  props: {
+    infoHeading: string;
+    img: (string | undefined)[];
+    paragraph: (string | undefined)[];
+  };
+}): JSX.Element {
+  const informationCardsData = [
+    { imgSrc: props.img[0], alt: "image1", pText: props.paragraph[0] },
+    { imgSrc: props.img[1], alt: "image2", pText: props.paragraph[1] },
+    { imgSrc: props.img[2], alt: "image3", pText: props.paragraph[2] },
+  ];
+
+  return (
+    <div className="gap-16 mx-4 space-y-2 md:space-y-0 md:grid md:grid-cols-3">
+      {informationCardsData.map((card) => (
+        <div className="w-full hover:shadow-2xl">
+          <img
+            src={card.imgSrc}
+            alt={card.alt}
+            className="border-4 border-blue-900 rounded-xl "
+          />
+          <p className="p-2">{card.pText}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ const paragraphText = [
   "Lorem ipsum dolor sit amet consectetur. Tempora, nihil quo. Aliquam excepturi quaerat exercitationem reprehenderit, maxime saepe! Nostrum repudiandae, amet culpa iusto recusandae aliquid. Dolor iure saepe nobis fugit.",
   "Lorem ipsum dolor sit amet, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   "Sign up for more lorem ipsum",
-] as const;
+];
 
 const links = ["link 1", "link 2", "link 3"] as const;
 
@@ -26,6 +26,6 @@ const img = [
   "https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   "https://images.unsplash.com/photo-1581276879432-15e50529f34b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
   "https://images.unsplash.com/photo-1569748130764-3fed0c102c59?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-] as const;
+];
 
 export { paragraphText, links, heading, text, img };
