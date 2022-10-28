046b1526a8b3c118417445ef47910731689bf2f1
milliorn <scottmilliorn@gmail.com>
2022-10-28 09:41:53 -0700

    Contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,7 @@
-export function Contact(props) {
+/**
+ * @param {{ h2: string; p: string; button: string; }} props
+ */
+export function Contact(props: { h2: string; p: string; button: string }): JSX.Element {
   return (
     <div className="top-0 left-0 w-full h-full p-12 text-sm text-center sm:text-base text-gray-50 bg-gray-50">
       <div className="relative p-4 mx-auto my-0 bg-blue-500 sm:w-11/12 h-3/6 top-1/4">
