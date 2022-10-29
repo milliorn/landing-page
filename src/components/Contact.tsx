3125427cb64b7bee1d3a96361da430671ea2a9ee
milliorn <scottmilliorn@gmail.com>
2022-10-28 19:08:46 -0700

    Missing type attribute in button elements

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,7 +11,10 @@ export function Contact(props: {
       <div className="relative p-4 mx-auto my-0 bg-gray-900 sm:w-11/12 h-3/6 top-1/4">
         <h2 className="m-4 font-bold capitalize">{props.h2}</h2>
         <p>{props.p}</p>
-        <button className="px-8 py-4 mx-auto my-4 capitalize bg-gray-50 border border-gray-900 shadow hover:animate-pulse hover:bg-blue-900 text-black hover:text-gray-50 rounded-xl hover:shadow-lg hover:border-transparent">
+        <button
+          type="button"
+          className="px-8 py-4 mx-auto my-4 capitalize bg-gray-50 border border-gray-900 shadow hover:animate-pulse hover:bg-blue-900 text-black hover:text-gray-50 rounded-xl hover:shadow-lg hover:border-transparent"
+        >
           {props.button}
         </button>
       </div>
