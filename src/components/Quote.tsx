284b91f2d8458cad03b6aefe698d9c340b66bd81
Bhumika Tiwari <156492366+bhumitiwari@users.noreply.github.com>
2025-02-26 04:39:55 +0530

    Added animation (#190)
    Added sliding animation to the quote.
    
    ---------
    
    Signed-off-by: Scott Milliorn <scottmilliorn@gmail.com>
    Co-authored-by: Scott Milliorn <scottmilliorn@gmail.com>

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,14 +1,21 @@
 /**
  * @param {{ quote: string; author: string; }} props
  */
+import { motion } from "framer-motion";
+
 export function Quote(props: { quote: string; author: string }): JSX.Element {
   return (
-    <div className="bg-gray-200">
-      <p className="p-24 m-auto text-xl italic font-light text-center text-gray-800 sm:text-4xl">
-        <span>{props.quote}</span>
-        <span className="ml-4 font-bold text-center capitalize sm:text-2xl">
-          - {props.author}
-        </span>
+    <div className="bg-gray-200 p-24">
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 5 }}
+        className="m-auto text-xl italic font-light text-center text-gray-800 sm:text-4xl"
+      >
+        {props.quote}
+      </motion.div>
+      <p className="mt-4 text-center font-bold capitalize sm:text-2xl">
+        - {props.author}
       </p>
     </div>
   );
