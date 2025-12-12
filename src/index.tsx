e5d7d5670047bc1421b031b72d73f0f82366348c
Scott Milliorn <scottmilliorn@gmail.com>
2025-12-12 07:55:01 -0800

    prettier

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,11 @@ import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
