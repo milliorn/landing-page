c7cbd2258e1cb68a6d3ea672c48b74ec97e05015
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-31 16:51:16 -0700

    Refactor NavBar component for improved readability and formatting

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,8 +1,7 @@
 export function Copyright({ owner }: { owner: string }) {
   return (
     <p className="p-8 text-center text-gray-50 hover:animate-pulse hover:underline">
-      <span className="text-lg">&copy;</span> {new Date().getFullYear()}{" "}
-      {owner}
+      <span className="text-lg">&copy;</span> {new Date().getFullYear()} {owner}
     </p>
   );
 }
